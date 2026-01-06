ef95d7abc500a022ba0503e970c29b7dcbfe89b9
TOIKIT <develop@toikit.com>
2026-01-06 20:12:49 +0700

    Update var type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ref, computed, watch } from 'vue';
+import { ref, watch } from 'vue';
 
 /** Base URL template for locale modules; replace `{locale}` when loading */
 let localeModuleTemplates = ref<Array<any>>([]);
